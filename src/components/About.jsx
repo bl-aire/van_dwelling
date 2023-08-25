21a0539b20d5b8bb8d26765ba2de80dffa8790a9
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-08-25 19:10:21 +0100

    Style About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,24 @@
+import home  from "../assets/home.png"
 
 export default function About(){
     return(
         <div>
-            About
+            <div>
+                <img src={home} className="about-image"/>
+            </div>
+            <div className="about-text_container">
+                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                <p>
+                    Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉) Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+                </p>
+                <div className="explore-container">
+                    <h2>
+                        Your destination is waiting.<br/>Your van is ready.
+                    </h2>
+                    <button>Explore our vans</button>
+                </div>
+            </div>
+
         </div>
     )
 }
