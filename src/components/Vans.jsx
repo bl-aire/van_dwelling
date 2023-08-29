e6a9a5de501a401bb8ae1740030ee43724ab3627
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-08-29 22:54:44 +0100

    Use data to create vans component

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,7 +1,41 @@
-export default function Vans(){
+import React from "react"
+
+export default function Vans() {
+
+        
+    const [vans, setVans] = React.useState([])
+    
+    const fetchVans = () => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+        .catch(error => console.log(error))
+    }
+    
+    React.useEffect(() => {
+        fetchVans()
+    }, [])
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>
+                    ${van.price}
+                    <span>/day</span>
+                </p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
+
     return(
-        <section>
-            Vans
+        <section className="vans-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
         </section>
     )
 }
