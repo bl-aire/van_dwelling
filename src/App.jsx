cdecd5b2561d7b2212f8175214199a64d778f753
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-09-07 15:00:47 +0100

    Fix header and footer using Layout Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./server"
 import './App.css'
-import Head from './components/Head'
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans from './pages/Vans'
 import VanDetail from './pages/VanDetail'
-import Foot from './components/Foot'
+import Layout from './components/Layout'
 
 function App() {
   return(
     <>
       <BrowserRouter>
-        <Head/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/vans" element={<Vans/>}/>
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/vans" element={<Vans/>}/>
+            <Route path="/vans/:id" element={<VanDetail />}/>
+          </Route>
         </Routes>
-        <Foot/>
       </BrowserRouter>
     </>
   )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,13 @@
+import Foot from "./Foot"
+import Head from "./Head"
+import { Outlet } from "react-router-dom"
+
 export default function Layout(){
     return(
-        <div></div>
+        <div>
+            <Head/>
+            <Outlet/>
+            <Foot/>
+        </div>
     )
 }
